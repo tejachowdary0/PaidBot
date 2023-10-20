ee8f0cef2f23b4d905d8449fe904afadc02a0a58
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-10-20 18:08:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone https://github.com/Vijay14958/PaidBot /PaidBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /PaidBot
 fi
-cd /Advance-Auto-Filter
+cd /PaidBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
